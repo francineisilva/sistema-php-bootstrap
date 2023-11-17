a1077cccd3251eb774af26e3423886238d72f020
Francinei Silva <francineisilva7@gmail.com>
2023-11-17 15:54:47 -0300

    login.php
    Criando a tela de login

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">  
+<head>  <!--Definição de TAG | Existe dois tipos de tag <abertas> abertas e </fechadas>-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">  <!-- Tag para chamar o css da pasta "css/login.css"-->
+    <title>Acesso ao sistema</title> <!-- Tag para alterar o título da página-->
+</head>
+<body>
+    <h1>Iniciando um projeto em PHP</h1>
+</body>
+</html>
