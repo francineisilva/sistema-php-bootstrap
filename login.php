bd72e825fade388a253ab2f95f5cbdd29c61065c
Francinei Silva <francineisilva7@gmail.com>
2023-11-17 21:03:41 -0300

    Criação da tela de login

--- login.php
+++ login.php
@@ -7,6 +7,30 @@
     <title>Acesso ao sistema</title> <!-- Tag para alterar o título da página-->
 </head>
 <body>
-    <h1>Iniciando um projeto em PHP</h1>
+    <div class="wrapper">
+        <div class="form-container sign-in">
+            <form action="#">
+                <h2>Acesso ao sistema</h2>
+                <div class="form-group">
+                    <input type="text" name="" id="" required>
+                    <i class="fas fa-user"></i>
+                    <label for="">Usuário</label>
+                </div>
+                <div class="form-group">
+                    <input type="password" name="" id="" required>
+                    <i class="fas fa-lock"></i>
+                    <label for="">Senha</label>
+                </div>
+                <div class="forgot-pass">
+                    <a href="#"></a>Esqueceu sua senha?</div>
+                    <button type="submit" class="btn">Acessar</button>
+                    <div class="link">
+                        <p>Ainda não tem uma conta?<a href="#" class="cadastrar-link">Cadastre-se</a></p>
+                    </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/3119050dfa.js" crossorigin="anonymous"></script>
+
 </body>
 </html>
